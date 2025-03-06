a64b786f02fcc50b6496a87a3d9f1ce270b29911
fatima zahra cherkaoui <cherkaoui.fatimazahra97@gmail.com>
2025-03-06 14:06:59 +0000

    Add screens for adding recipes, home, and recipe details

---
+++ src/screens/AddRecipeScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AddRecipeScreen() {
+  return (
+    <View>
+      <Text>AddRecipeScreen Screen</Text>
+    </View>
+  );
+}
+
+export default AddRecipeScreen
---
+++ src/screens/RecipeDetailScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RecipeDetailScreen() {
+  return (
+    <View>
+      <Text>RecipeDetailScreen</Text>
+    </View>
+  )
+}
+
+export default RecipeDetailScreen
